ad83280004fea220d1341d79f9ae9ca3331cd840
Chohyejin53 <hyejin2338@gmail.com>
2024-03-13 13:24:13 +0900

    add data

--- src/constants/CareerList.jsx
+++ src/constants/CareerList.jsx
@@ -1,19 +1,19 @@
 export const CAREERLIST01 = [
     {
         num: "001",
-        tag: ["Blog", "maintenance"],
-        period: "Sept.2022",
-        orderingDepartment: "NaverBlog",
-        link: "campaign.naver.com/blogpeople/",
-        project: "블로그피플 작가인터뷰 신규 마크업"
+        tag: ["VITA", "new"],
+        period: "Mar.2022",
+        orderingDepartment: "SNOW",
+        link: "vday.io/event/thumbnail",
+        project: "유튜브 썸네일 이벤트 프로모션 마크업"
     },
     {
         num: "002",
-        tag: ["Blog", "maintenance"],
-        period: "Sept.2022",
-        orderingDepartment: "NaverBlog",
-        link: "campaign.naver.com/blogpeople/",
-        project: "블로그피플 작가인터뷰 신규 마크업"
+        tag: ["SME", "new"],
+        period: "Mar.2022",
+        orderingDepartment: "Smart Place",
+        link: "https://campaign.naver.com/smp_loan_care/",
+        project: "마트 플레이스 대출안심케어 신규 마크업"
     },
     {
         num: "003",
