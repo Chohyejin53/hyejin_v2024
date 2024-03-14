ae35c1aba533101d8cb921851a820ad660f6c9d2
Chohyejin53 <hyejin2338@gmail.com>
2024-03-14 23:53:41 +0900

    modify stack section

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -6,13 +6,13 @@ import '../asset/styles/portfolio_v4.scss'
 
 const Stack = () => {
     const horizontalRef = useRef(null);
-    const sectionRef = useRef([]);
+    const sectionRefs = useRef([]);
 
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
 
         const horizontal = horizontalRef.current;
-        const sections = sectionRef.current;
+        const sections = sectionRefs.current;
 
         let scrollTween = gsap.to(sections, {
             xPercent: -120 * (sections.length - 1),
@@ -34,11 +34,13 @@ const Stack = () => {
         };
     }, []);
 
-    const [flippedIndex, setFlippedIndex] = useState(null);
+    const [isFlipped, setIsFlipped] = useState([]);
 
-  const handleCardClick = () => {
-    setFlippedIndex((prevState) => !prevState);
-  };
+    const handleFlip = (index) => {
+        const flippedState = [...isFlipped];
+        flippedState[index] = !flippedState[index];
+        setIsFlipped(flippedState);
+    };
 
     
     return (
@@ -51,10 +53,10 @@ const Stack = () => {
                 <div className="stack__wrap">
                     {STACKLIST.map((stack, key) => (
                         <div 
-                            className={`stack__item p${key + 1} ${flippedIndex === key ? 'on' : ''} card_item` }
+                            className={`stack__item p${key + 1} ${isFlipped[key] ? 'flipped' : ''} card_item` }
                             key={key}
-                            ref={(el) => (sectionRef.current[key] = el)}
-                            onClick={handleCardClick}
+                            ref={(el) => (sectionRefs.current[key] = el)}
+                            onClick={() => handleFlip(key)}
                         >
                             <span className="num">{stack.num}.</span>
                             <div className="front_card">
@@ -63,18 +65,11 @@ const Stack = () => {
                                     className="img front_card" 
                                     rel="noreferrer noopener"
                                 >
-                                    <img src={stack.img} alt={stack.name} />
+                                    <h3 className="title">{stack.title}</h3>
                                 </button>
                             </div>
                             <div className="back_card">
-                                <h3 className="title">{stack.title}</h3>
                                 <p className="desc">{stack.desc}</p>
-                                <a 
-                                    href={stack.view} 
-                                    className="site" 
-                                    rel="noreferrer noopener"
-                                >
-                                    사이트 보기</a>
                             </div>
                         </div>
                     ))}
